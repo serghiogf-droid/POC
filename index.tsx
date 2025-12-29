0ffa2369fe62a05de84ff1066f54a64d6b781183
serghiogf-droid <serghio.gf@gmail.com>
2025-12-29 15:19:27 -0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -8,9 +7,14 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
+console.log("Index loaded OK"); // <-- agrega esto
+
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <div style={{ padding: 20 }}>
+      <h1>Mounted OK</h1>
+      <App />
+    </div>
   </React.StrictMode>
 );
